11799631c1e835c1d34bd6b1051319274bceae7f
lihan <frankieteardrop@gmail.com>
2012-11-12 13:28:54 -0500

    better terminal colors

--- osx.bash
+++ osx.bash
@@ -2,13 +2,16 @@ export PATH=$PATH:/usr/local/mysql/bin
 alias gd='git diff | subl -n'
 alias gdc='ga .; git diff --cached | subl -n'
 
-#enables color in the terminal bash shell export
-CLICOLOR=1
-#sets up the color scheme for list export
-LSCOLORS=gxfxcxdxbxegedabagacad
-#enables color for iTerm
-export TERM=xterm-color
-#sets up proper alias commands when called
+# enable colors
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+  export TERM='xterm-256color'
+else
+  export TERM='xterm-color'
+fi
+
+# sets up proper alias commands when called
 alias ls='ls -G'
 alias ll='ls -hl -la'
 
