be44e0d823ed70135081dc18fdf376373e9ce534
Lihan Li <frankieteardrop@gmail.com>
2013-01-12 19:44:45 -0500

    add dummy secret token for dev use

--- configs/osx.bash
+++ configs/osx.bash
@@ -22,5 +22,8 @@ alias fdns='sudo killall -HUP mDNSResponder'
 
 export EDITOR='subl -w -n'
 
+# rails secret token (only use in development env)
+export SECRET_TOKEN='e809a0d9ef130a5f07630655e6b3ca15f3eef3a2de38d3de68857972237f204ed5022507ae3340c03570cf92e81eb8fc9b619b87a50d9ad7ae335b56c28f0438'
+
 # add keys to agent by default
 ssh-add > /dev/null 2>&1
