c15d1dfb7015f8258c583c647285bd231049d3c1
Lihan Li <frankieteardrop@gmail.com>
2014-04-23 19:55:23 -0400

    duplicate usr local bin path prepend

--- configs/osx.bash
+++ configs/osx.bash
@@ -2,8 +2,6 @@ source $DOTFILES_CONFIGS_DIR/unix_gui.bash
 unalias subl
 export EDITOR='subl -n -w'
 
-path_prepend '/usr/local/bin'
-
 path_append '/usr/local/mysql/bin'
 path_append '/Applications/Postgres.app/Contents/MacOS/bin'
 path_append '/Applications/Postgres93.app/Contents/MacOS/bin'
