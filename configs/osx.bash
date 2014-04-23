ac5ac875896a15299a78f4383ba5a6417551d692
Lihan Li <frankieteardrop@gmail.com>
2014-04-22 23:23:56 -0400

    todo finished

--- configs/osx.bash
+++ configs/osx.bash
@@ -8,6 +8,4 @@ path_append '/usr/local/mysql/bin'
 path_append '/Applications/Postgres.app/Contents/MacOS/bin'
 path_append '/Applications/Postgres93.app/Contents/MacOS/bin'
 
-# TODO git zsh completion
-
 alias fdns='sudo killall -HUP mDNSResponder'
