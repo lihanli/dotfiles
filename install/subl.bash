24714bc1f593bd3e6bbe1d7fe02eeebba372537d
Lihan Li <frankieteardrop@gmail.com>
2016-07-28 14:01:23 -0400

    upgrade to st3 osx

--- install/subl.bash
+++ install/subl.bash
@@ -2,7 +2,7 @@
 if [ -d ~/.config/sublime-text-3 ]; then
   sublime_dir=~/.config/sublime-text-3
 else
-  sublime_dir="$HOME/Library/Application Support/Sublime Text 2"
+  sublime_dir="$HOME/Library/Application Support/Sublime Text 3"
 fi
 # if directory doesnt exist then exit
 user_dir=$sublime_dir/Packages/User
@@ -11,4 +11,8 @@ user_dir=$sublime_dir/Packages/User
 if [[ ! -L $user_dir ]]; then
   mv "$user_dir" "$sublime_dir/Packages/User_old"
   ln -s "$dotfiles_dir/sublime/User" "$user_dir"
+fi
+# osx subl shortcut
+if [[ $(os_detect) = osx ]]; then
+  ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 fi
