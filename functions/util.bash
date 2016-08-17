a208ff6fc08a5de352a8235a8818ea55ed721f4b
Lihan Li <frankieteardrop@gmail.com>
2016-08-16 20:59:22 -0400

    use xorg to check if ubuntu is desktop

--- functions/util.bash
+++ functions/util.bash
@@ -2,7 +2,7 @@ os_detect() {
   if [[ $OSTYPE =~ ^darwin.*$ ]]; then
     echo 'osx'
   else
-    dpkg -l ubuntu-desktop > /dev/null 2>&1
+    dpkg -l xorg > /dev/null 2>&1
 
     if [[ $? == 0 ]]; then
       echo 'ubuntu-desktop'
