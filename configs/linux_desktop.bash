9290fe5d409eb2dc29c6e24383342bfe9ca6474c
Lihan Li <frankieteardrop@gmail.com>
2017-02-11 17:17:36 -0500

    add pbcopy + paste

--- configs/linux_desktop.bash
+++ configs/linux_desktop.bash
@@ -14,6 +14,8 @@ alias gd="pipe_into_editor git diff"
 alias gdc="ga --all .; pipe_into_editor git diff --cached"
 alias chrome='google-chrome'
 alias rr='pipe_into_editor rc rake routes'
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
 
 function gds {
   pipe_into_editor git diff $1..$2
