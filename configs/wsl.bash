037cf314ffd5f88db430452e0994040ebf6da8a7
Lihan Li <frankieteardrop@gmail.com>
2022-01-29 17:53:25 -0800

    add open common docs func

--- configs/wsl.bash
+++ configs/wsl.bash
@@ -2,6 +2,10 @@ function subl() {
   nohup /mnt/c/Program\ Files/Sublime\ Text/sublime_text.exe -n "$(convert_linux_path $1)" >/dev/null 2>&1 &
 }
 
+function subl_win_path() {
+  nohup /mnt/c/Program\ Files/Sublime\ Text/sublime_text.exe "$1" >/dev/null 2>&1 &
+}
+
 WSL_PREFIX='\\wsl$\Ubuntu-'
 VERSION=$(lsb_release -a 2> /dev/null | grep Release | awk -F ' ' '{print $NF}')
 export WSL_PATH_PREFIX="$WSL_PREFIX$VERSION"
@@ -23,4 +27,12 @@ function spf() {
 
 function gds {
   pipe_into_editor git diff $1..$2
+}
+
+function open_common_docs() {
+  subl
+  sleep 2
+  subl_win_path 'C:\Users\lihan\Google Drive\files\documents\mfd.txt'
+  subl_win_path 'C:\Users\lihan\Google Drive\files\documents\todo.txt'
+  subl_win_path 'C:\Users\lihan\Google Drive\files\sysadmin\norgate update.txt'
 }
