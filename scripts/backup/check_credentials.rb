b0d49d23257e1432fd04614cb407fa464d9eb91a
lihan li <frankieteardrop@gmail.com>
2024-05-02 18:44:36 +0700

    fix check crednetials

--- scripts/backup/check_credentials.rb
+++ scripts/backup/check_credentials.rb
@@ -1,2 +1,2 @@
 raise 'missing aws' unless `aws --version 2>&1`.include?('aws-cli')
-raise 'wrong credentials' unless system('aws s3api list-objects --bucket lihan > /dev/null')
+raise 'wrong credentials' unless system('aws s3 ls s3://lihan > /dev/null')
