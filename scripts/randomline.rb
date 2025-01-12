6b7598ff2eb3560dd13d97d2fe3e9e93a6bf4ce9
lihan <frankieteardrop@gmail.com>
2025-01-13 03:45:46 +0700

    fix random line

--- scripts/randomline.rb
+++ scripts/randomline.rb
@@ -1,5 +1,5 @@
 # Open the file and read all lines
-lines = File.readlines(ARGV[0])
+lines = File.readlines(ARGV[0]).reject { |line| line.strip.empty? }.reject { |line| ['<highlight>', '/<highlight>'].include?(line) }
 
 # Pick a random line
 random_line = lines.sample
